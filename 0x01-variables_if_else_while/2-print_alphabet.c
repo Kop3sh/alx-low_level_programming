7b18357e405d679f4bd31083784faf3052e1bd88
Kop3sh <kirollosadel24@yahoo.com>
2022-12-09 20:31:47 +0200

    -m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char i;
+
+	i = 'a';
+
+	while (i < 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
