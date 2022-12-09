e9ce52259c5244627dcbfe5482ede1dfa1faa8bc
Kop3sh <kirollosadel24@yahoo.com>
2022-12-09 20:54:11 +0200

    -m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('$');
+	putchar('\n');
+	return (0);
+}
