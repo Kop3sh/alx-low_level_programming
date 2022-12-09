4188058fba89ee243836d9f1457140ce867831da
Kop3sh <kirollosadel24@yahoo.com>
2022-12-09 20:40:40 +0200

    -m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char i;
+
+	i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i == 'q' || i == 'e')
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
