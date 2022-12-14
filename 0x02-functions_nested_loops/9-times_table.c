166bc3947768d75e6b12e8b88aee203b4766fef6
Kop3sh <kirollosadel24@yahoo.com>
2022-12-14 02:27:09 +0200

    -m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -41,26 +41,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
-
-void times_table(void)
-{
-	int i, j, k;
-
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 0; i < 10; i++)
-		{
-			k = i * j;
-			_putchar(k + '0');
-			if (i < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (k < 10)
-					_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
-}
