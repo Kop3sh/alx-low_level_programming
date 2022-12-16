ee6bcafe85b9b2320231847e67c5026e7f4a1df4
Kop3sh <kirollosadel24@yahoo.com>
2022-12-16 23:44:31 +0200

    -m

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print decimal numbers fromo 0 to 9
+ * @n: number of lines
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i < j; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+}
+
