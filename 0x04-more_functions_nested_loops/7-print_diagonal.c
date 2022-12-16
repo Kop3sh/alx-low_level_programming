a32d4862aff4355f033e8ec415b9fcde8b25f4d7
Kop3sh <kirollosadel24@yahoo.com>
2022-12-16 23:53:49 +0200

    -m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,6 +10,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
+
 	for (j = 0; j < n; j++)
 	{
 		for (i = 0; i < j; i++)
@@ -19,7 +22,5 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
 
