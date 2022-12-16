74255828156bffdefb0eff396575790b9a4fc0d0
Kop3sh <kirollosadel24@yahoo.com>
2022-12-16 23:33:03 +0200

    -m

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print decimal numbers fromo 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i,j;
+
+	for (j = 0; i < 10; i++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+	_putchar('\n');
+	}
+	_putchar('\n');
+
+}
+
