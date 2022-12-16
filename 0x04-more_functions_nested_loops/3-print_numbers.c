f403a0ecfa8b069913dd9f8ac5aa3dca4426b228
Kop3sh <kirollosadel24@yahoo.com>
2022-12-16 23:26:39 +0200

    -m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
-/*
- * print_numbers - check uppercase letters
+/**
+ * print_numbers - print decimal numbers fromo 0 to 9
  *
- *
- * Return: alwyas 0
+ * Return: void
  */
 void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
 	}
