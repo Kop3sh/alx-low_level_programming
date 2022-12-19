aa4464bb1882ae0c39b909faa73f6bbcf8f5cded
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 18:34:24 +0200

    -m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - rests any number by reference to 98
+* @a: input by reference
+* @n: length of arr
+*
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if ( i == n - 1)
+		{
+			printf("%d\n", a[i]);
+			continue;
+		}
+		printf("%d, ", a[i]);
+	}
+}
