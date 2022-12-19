c0b2bcbb98c44a382b359406917450f0470d9992
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 17:51:59 +0200

    -m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
 */
 void print_rev(char *s)
 {
-	int len = _strlen(s);
+	int len = 0, i = 0;
 
-	while (len)
-	{
-		_putchar(s[len - 1]);
+	while (s[i++])
 		len--;
-	}
+
+	for (i = len - 1; i <= 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
