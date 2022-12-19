3c5dfbb54d0d89df1938acb7e505d99a7237e14b
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 17:22:26 +0200

    -m

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print decimal numbers fromo 0 to 9
+ * @n: input number
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	while (i > 9)
+	{
+		:wq
+		n /= 10;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+
+}
+
