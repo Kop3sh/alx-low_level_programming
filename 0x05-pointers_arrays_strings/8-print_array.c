daf54db2e1931ae66d223560a6ad3f2e5c43e337
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 18:36:40 +0200

    -m

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if ( i == n - 1)
+		if (i == n - 1)
 		{
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 			continue;
 		}
 		printf("%d, ", a[i]);
 	}
+	_putchar('\n');
 }
