5f714cd10b16d259bf4e5070aa75fe16939fe563
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 18:22:13 +0200

    -m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,16 @@
 */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
-	while (str[i] != '\0')
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i++)
 	{
-		if (i == 0)
-			_putchar(str[i]);
-		else
-			_putchar(str[++i]);
-		i++;
+		if (i % 2)
+			continue;
+		_putchar(str[i]);
 	}
 
 	_putchar('\n');
