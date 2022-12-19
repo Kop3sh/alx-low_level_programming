dfa416a0f09d9a7bea6a94f08d14519256b85858
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 18:00:30 +0200

    -m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,15 @@
 void rev_string(char *s)
 {
 	int len = 0, i = 0;
-	char *tmp = NULL;
+	char tmp;
 
 	while (s[i++])
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
-		tmp[len - i - 1] = s[i];
-
-	_putchar('\n');
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }
