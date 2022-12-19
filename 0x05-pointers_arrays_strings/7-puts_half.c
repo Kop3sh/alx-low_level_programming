810c862e6059e6c7c673150bce9a338317241c26
Kop3sh <kirollosadel24@yahoo.com>
2022-12-19 18:27:20 +0200

    -m

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,17 @@
 */
 void puts_half(char *str)
 {
-	int i = 0, len = 0;
+	int n, i = 0, len = 0;
 
 	while (str[i++])
 		len++;
+	
+	if (len % 2)
+		n = (len - 1) / 2;
+	else
+		n = len / 2;
 
-	for (i = (len - 1) / 2; i < len; i++)
+	for (i = n; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
