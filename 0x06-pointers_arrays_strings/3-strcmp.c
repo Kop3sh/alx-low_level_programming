f3c6884d8e3790eaea5a1c2393a388b5e9630498
Kop3sh <kirollosadel24@yahoo.com>
2022-12-22 17:51:13 +0200

    -m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, flag =  1;
+	int i = 0, diff =  0;
 
 	for (i = 0; s2[i] != '\0' && s1[i] != '\0'; i++)
 		if (s2[i] != s1[i])
-			flag = 0;
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
 
-
-	return (flag);
+	return (diff);
 }
