3c5824d21ab0f3bbd0fe6d179ae159e24b042960
Kop3sh <kirollosadel24@yahoo.com>
2022-12-22 18:04:11 +0200

    -m

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,19 @@
 
 
 /**
-* reverse_array - rests any number by reference to 98
-* @int: input by reference
-* @j: input by reference
+* string_toupper - rests any number by reference to 98
+* @str: input by reference
 *
 * Return: void
 */
-void reverse_array(int *a, int n)
+char *string_toupper(char *str)
 {
-	int tmp, i = 0;
+	int i;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		str[i] -= 32;
 	}
+
+	return (str);
 }
