0ad1344f33bb396f49da20cd19772d2b66fb728c
Kop3sh <kirollosadel24@yahoo.com>
2022-12-22 17:30:46 +0200

    -m

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+* _strcat - rests any number by reference to 98
+* @dest: input by reference
+* @src: input by reference
+*
+* Return: char dest ptr
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0;i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
