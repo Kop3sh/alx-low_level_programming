8466c05b73f901900d1d8870c54b5df483c7252b
Kop3sh <kirollosadel24@yahoo.com>
2022-12-22 18:24:26 +0200

    -m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,18 +2,35 @@
 
 
 /**
-* string_toupper - rests any number by reference to 98
+* cap_string - rests any number by reference to 98
 * @str: input by reference
 *
 * Return: char 
 */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i - 1] != '\0'; i++)
 	{
-		str[i] -= 32;
+		if (!(str[i - 1] >= 'a' && str[i] <= 'z'))
+			continue;
+
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
 	}
 
 	return (str);
