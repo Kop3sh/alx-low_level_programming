3ff5ecd86241eb15d53fb1337b07525a10675cc7
Kop3sh <kirollosadel24@yahoo.com>
2023-01-02 19:36:00 +0200

    -m

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_chessboard - rests any number by reference to 98
+* @a: memroy pointed by s
+*
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+	}
+}
