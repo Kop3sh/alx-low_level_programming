ae317caf1277bb70a6e57603bfb3f8c961608556
Kop3sh <kirollosadel24@yahoo.com>
2023-01-02 18:59:32 +0200

    -m

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - rests any number by reference to 98
+* @s: memroy pointed by s
+* @c: const byte
+*
+* Return: char *
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+
+	return (NULL);
+}
