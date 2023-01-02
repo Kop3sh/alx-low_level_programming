01bec234869f72d91ceb2749cc6529cac43c4d86
Kop3sh <kirollosadel24@yahoo.com>
2023-01-02 20:10:15 +0200

    -m

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string - rests any number by reference to 98
+* @s: memroy pointed by s
+* @to: memroy pointed by s
+*
+* Return: void
+*/
+void set_string(char **s, char *to)
+{
+	to = *s;
+}
