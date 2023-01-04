ff49da053f8bb9467ecba4c04257e125d1ee5725
Kop3sh <kirollosadel24@yahoo.com>
2023-01-04 19:05:48 +0200

    -m

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts_recursioon - rests any number by reference to 98
+* @s: memroy pointed by s
+*
+* Return: void
+*/
+void _puts_recursioon(char *s)
+{
+	int i = 0;
+
+	if (s)
+	{
+		_putchar(*(s++));	
+	}
+}
