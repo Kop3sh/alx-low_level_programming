8c8658ddbc33236ac966144abdf4887a448827db
Kop3sh <kirollosadel24@yahoo.com>
2023-01-04 19:24:54 +0200

    -m

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - rests any number by reference to 98
+* @s: memroy pointed by s
+*
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+	}
+
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - rests any number by reference to 98
+* @s: memroy pointed by s
+*
+* Return: void
+*/
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+		len += _strlen_recursion(s + 1);
+	return (len);
+
+}
