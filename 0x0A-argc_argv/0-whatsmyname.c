6137c21d62daa0d6fb861751dbe2857c87e36bb9
Kop3sh <kirollosadel24@yahoo.com>
2023-01-06 19:41:10 +0200

    -m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: num of cli parameters
+ * @argv: arr of cli parameter values
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
