5901c991b0ad4d2793f2808701f6e412b73b0127
Kop3sh <kirollosadel24@yahoo.com>
2023-01-06 19:52:45 +0200

    -m

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: num of cli parameters
+ * @argv: arr of cli parameter values
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
