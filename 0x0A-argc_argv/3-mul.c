2ec630b65ff0535f3ecd9e0c6fb76d607b3dfdb8
Kop3sh <kirollosadel24@yahoo.com>
2023-01-06 19:58:44 +0200

    -m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: num of cli parameters
+ * @argv: arr of cli parameter values
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, prod = 0;
+
+	if (argc != 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+		prod = prod * atoi(argv[i]);
+
+	printf("%d", prod);
+
+	return (0);
+}
