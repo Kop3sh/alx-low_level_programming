375d43b06446b4194b73b48c09885e23584f47f2
Kop3sh <kirollosadel24@yahoo.com>
2023-01-09 19:28:20 +0200

    -m

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* create_array - rests any number by reference to 98
+* @c: memroy pointed by s
+* @size: size of array to be allocatoed
+*
+* Return: void
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (! size)
+		return ("NULL");
+
+	arr = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	
+	return (arr);
+}
