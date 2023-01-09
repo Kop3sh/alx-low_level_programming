196f6f92edc46bd70e1260348f5332ab1ecbbdbc
Kop3sh <kirollosadel24@yahoo.com>
2023-01-09 19:28:53 +0200

    -m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (! size)
+	if (!size)
 		return ("NULL");
 
 	arr = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	
+
 	return (arr);
 }
