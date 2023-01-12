8a9613aa05d549926092997a4b3991ac9f7f272a
Kop3sh <kirollosadel24@yahoo.com>
2023-01-12 14:44:02 +0200

    -m

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* string_nconcat - rests any number by reference to 98
+* @b: memroy pointed by s
+*
+* Return: void
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned i, str_idx = 0, len_s2 = 0;
+
+	if (n <= 0)
+	{
+		return (NULL);	
+	}
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	for (i = 0; s2[i]; i++)
+		len_s2++;
+
+	str = malloc((++n * sizeof(char)) + sizeof s1);
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		str[str_idx++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		str[str_idx++] = s2[i];
+	
+	str[str_idx] = '\0';
+	return (str);
+}
