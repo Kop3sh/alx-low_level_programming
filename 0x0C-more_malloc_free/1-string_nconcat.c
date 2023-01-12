3874f33c0fb8951facbe07d11ad90d52c14db446
Kop3sh <kirollosadel24@yahoo.com>
2023-01-12 14:52:59 +0200

    -m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,29 +4,29 @@
 
 /**
 * string_nconcat - rests any number by reference to 98
-* @b: memroy pointed by s
+* @s1: memroy pointed by s
+* @s2: memroy pointed by s
+* @n: number of chars to be concatenaged
 *
 * Return: void
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned i, str_idx = 0, len_s2 = 0;
+	unsigned int i, str_idx = 0, len_s2 = 0;
 
 	if (n <= 0)
-	{
-		return (NULL);	
-	}
+		return (NULL);
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	for (i = 0; s2[i]; i++)
 		len_s2++;
 
-	str = malloc((n * sizeof(char)) + sizeof s1);
+	str = malloc((n * sizeof(char)) + sizeof(s1));
 
 	if (!str)
 		return (NULL);
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s2[i] && i < n; i++)
 		str[str_idx++] = s2[i];
-	
+
 	str[str_idx] = '\0';
 	return (str);
 }
